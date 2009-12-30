06446b62bca0dd0b93264753244dae7fbe6a35c7
Daniel Berger <dberger@globe.gov>
2009-12-29 23:01:34 -0700

    Updated platform checking.

--- test/test_sys_filesystem.rb
+++ test/test_sys_filesystem.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.dirname(File.expand_path(__FILE__))
    
 require 'rbconfig'
 
-if Config::CONFIG['host_os'].match('mswin')
+if Config::CONFIG['host_os'] =~ /mswin32|mingw|cygwin|windows|dos/i
    require 'test_sys_filesystem_windows'
 else
    require 'test_sys_filesystem_unix'
