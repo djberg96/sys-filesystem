91000d5b404c9a4484213c5ff953c91225344839
Daniel Berger <djberg96@gmail.com>
2012-01-04 17:52:58 -0700

    Added initial implementation of Filesystem.mounts and added internal classes Stat and Mount.

--- lib/unix/sys/filesystem.rb
+++ lib/unix/sys/filesystem.rb
@@ -5,10 +5,22 @@ module Sys
     extend FFI::Library
     ffi_lib(FFI::Library::LIBC)
 
+    private
+
     attach_function(:statvfs, [:string, :pointer], :int)
     attach_function(:strerror, [:int], :string)
 
-    class Error < StandardError; end
+    attach_function(:setmntent, [:string, :string], :pointer)
+    attach_function(:endmntent, [:pointer], :int)
+    attach_function(:getmntent, [:pointer], :pointer)
+
+    if File.exists?('/etc/mtab')
+      MOUNT_FILE = '/etc/mtab'
+    elsif File.exists?('/etc/mnttab')
+      MOUNT_FILE = '/etc/mnttab'
+    else
+      MOUNT_FILE = 'getmntinfo'
+    end
 
     class Statvfs < FFI::Struct
       layout(
@@ -29,32 +41,71 @@ module Sys
       )
     end
 
-    attr_accessor :block_size
-    attr_accessor :fragment_size
-    attr_accessor :blocks
-    attr_accessor :blocks_free
-    attr_accessor :blocks_available
-    attr_accessor :files
-    attr_accessor :files_free
-    attr_accessor :files_available
-    attr_accessor :filesystem_id
-    attr_accessor :flags
-    attr_accessor :name_max
-    attr_accessor :base_type
+    class Mntent < FFI::Struct
+      layout(
+        :mnt_fsname, :string,
+        :mnt_dir, :string,
+        :mnt_type, :string,
+        :mnt_opts, :string,
+        :mnt_freq, :int,
+        :mnt_passno, :int
+      )
+    end
 
-    def initialize
-      @block_size       = nil
-      @fragment_size    = nil
-      @blocks           = nil
-      @blocks_free      = nil
-      @blocks_available = nil
-      @files            = nil
-      @files_free       = nil
-      @files_available  = nil
-      @filesystem_id    = nil
-      @flags            = nil
-      @name_max         = nil
-      @base_type        = nil
+    public
+
+    class Error < StandardError; end
+
+    class Stat
+      attr_accessor :path
+      attr_accessor :block_size
+      attr_accessor :fragment_size
+      attr_accessor :blocks
+      attr_accessor :blocks_free
+      attr_accessor :blocks_available
+      attr_accessor :files
+      attr_accessor :files_free
+      attr_accessor :files_available
+      attr_accessor :filesystem_id
+      attr_accessor :flags
+      attr_accessor :name_max
+      attr_accessor :base_type
+
+      def initialize
+        @path             = nil
+        @block_size       = nil
+        @fragment_size    = nil
+        @blocks           = nil
+        @blocks_free      = nil
+        @blocks_available = nil
+        @files            = nil
+        @files_free       = nil
+        @files_available  = nil
+        @filesystem_id    = nil
+        @flags            = nil
+        @name_max         = nil
+        @base_type        = nil
+      end
+    end
+
+    class Mount
+      attr_accessor :name
+      attr_accessor :mount_point
+      attr_accessor :mount_type
+      attr_accessor :options
+      attr_accessor :mount_time
+      attr_accessor :dump_frequency
+      attr_accessor :pass_number
+
+      def initialize
+        @name = nil
+        @mount_point = nil
+        @mount_type = nil
+        @options = nil
+        @mount_time = nil
+        @dump_frequency = nil
+        @pass_number = nil
+      end
     end
 
     def self.stat(path)
@@ -64,7 +115,8 @@ module Sys
         raise Error, 'statvfs() function failed: ' + strerror(FFI.errno)
       end
 
-      obj = self.new
+      obj = Sys::Filesystem::Stat.new
+      obj.path = path
       obj.block_size = fs[:f_bsize]
       obj.fragment_size = fs[:f_frsize]
       obj.blocks = fs[:f_blocks]
@@ -79,6 +131,38 @@ module Sys
       obj.base_type = fs[:f_basetype]
 
       obj.freeze
+    end
+
+    def self.mounts
+      array = block_given? ? nil : []
+
+      begin
+        fp = setmntent(MOUNT_FILE, 'r')
+
+        while ptr = getmntent(fp)
+          break if ptr.null?
+          mt = Mntent.new(ptr)
+
+          obj = Sys::Filesystem::Mount.new
+          obj.name = mt[:mnt_fsname]
+          obj.mount_point = mt[:mnt_dir]
+          obj.mount_type = mt[:mnt_type]
+          obj.options = mt[:mnt_opts]
+          obj.mount_time = nil
+          obj.dump_frequency = mt[:mnt_freq]
+          obj.pass_number = mt[:mnt_passno]
+
+          if block_given?
+            yield obj.freeze
+          else
+            array << obj.freeze
+          end
+        end
+      ensure
+        endmntent(fp) if fp && !fp.null?
+      end
+
+      array
     end
   end
 end
