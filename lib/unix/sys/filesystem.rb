e7b7ef82e1e5c0cd383ab8ce49b190fece35fdbd
Daniel Berger <djberg96@gmail.com>
2012-01-10 12:45:43 -0700

    Added mount point property names and their stringy equivalent. Still a bug somewhere.

--- lib/unix/sys/filesystem.rb
+++ lib/unix/sys/filesystem.rb
@@ -20,6 +20,62 @@ module Sys
       attach_function(:getmntinfo64, [:pointer, :int], :int)
     end
 
+    MNT_RDONLY      = 0x00000001      # read only filesystem
+    MNT_SYNCHRONOUS = 0x00000002      # file system written synchronously
+    MNT_NOEXEC      = 0x00000004      # can't exec from filesystem
+    MNT_NOSUID      = 0x00000008      # don't honor setuid bits on fs
+    MNT_NODEV       = 0x00000010      # don't interpret special files
+    MNT_UNION       = 0x00000020      # union with underlying filesystem
+    MNT_ASYNC       = 0x00000040      # file system written asynchronously
+    MNT_CPROTECT    = 0x00000080      # file system supports content protection
+    MNT_EXPORTED    = 0x00000100      # file system is exported
+    MNT_QUARANTINE  = 0x00000400      # file system is quarantined
+    MNT_LOCAL       = 0x00001000      # filesystem is stored locally
+    MNT_QUOTA       = 0x00002000      # quotas are enabled on filesystem
+    MNT_ROOTFS      = 0x00004000      # identifies the root filesystem
+    MNT_DOVOLFS     = 0x00008000      # FS supports volfs (deprecated)
+    MNT_DONTBROWSE  = 0x00100000      # file system is not appropriate path to user data
+    MNT_IGNORE_OWNERSHIP = 0x00200000 # VFS will ignore ownership information on filesystem objects
+    MNT_AUTOMOUNTED = 0x00400000      # filesystem was mounted by automounter
+    MNT_JOURNALED   = 0x00800000      # filesystem is journaled
+    MNT_NOUSERXATTR = 0x01000000      # Don't allow user extended attributes
+    MNT_DEFWRITE    = 0x02000000      # filesystem should defer writes
+    MNT_MULTILABEL  = 0x04000000      # MAC support for individual labels
+    MNT_NOATIME     = 0x10000000      # disable update of file access time
+
+    MNT_VISFLAGMASK = (
+      MNT_RDONLY | MNT_SYNCHRONOUS | MNT_NOEXEC |
+      MNT_NOSUID | MNT_NODEV | MNT_UNION |
+      MNT_ASYNC  | MNT_EXPORTED | MNT_QUARANTINE |
+      MNT_LOCAL  | MNT_QUOTA |
+      MNT_ROOTFS | MNT_DOVOLFS | MNT_DONTBROWSE |
+      MNT_IGNORE_OWNERSHIP | MNT_AUTOMOUNTED | MNT_JOURNALED |
+      MNT_NOUSERXATTR | MNT_DEFWRITE  | MNT_MULTILABEL | MNT_NOATIME | MNT_CPROTECT
+    )
+
+    @@opt_names = {
+      MNT_RDONLY           => 'read-only',
+      MNT_SYNCHRONOUS      => 'synchronous',
+      MNT_NOEXEC           => 'noexec',
+      MNT_NOSUID           => 'nosuid',
+      MNT_NODEV            => 'nodev',
+      MNT_UNION            => 'union',
+      MNT_ASYNC            => 'asynchronous',
+      MNT_CPROTECT         => 'content-protection',
+      MNT_EXPORTED         => 'exported',
+      MNT_QUARANTINE       => 'quarantined',
+      MNT_LOCAL            => 'local',
+      MNT_QUOTA            => 'quotas',
+      MNT_ROOTFS           => 'rootfs',
+      MNT_DONTBROWSE       => 'nobrowse',
+      MNT_IGNORE_OWNERSHIP => 'noowners',
+      MNT_AUTOMOUNTED      => 'automounted',
+      MNT_JOURNALED        => 'journaled',
+      MNT_NOUSERXATTR      => 'nouserxattr',
+      MNT_DEFWRITE         => 'defwrite',
+      MNT_NOATIME          => 'noatime'
+    }
+
     if File.exists?('/etc/mtab')
       MOUNT_FILE = '/etc/mtab'
     elsif File.exists?('/etc/mnttab')
@@ -215,7 +271,23 @@ module Sys
           obj.name = mnt[:f_mntfromname].to_s
           obj.mount_point = mnt[:f_mntonname].to_s
           obj.mount_type = mnt[:f_fstypename].to_s
-          obj.options = mnt[:f_flags] # TODO: convert to strings
+
+          opts  = ""
+          flags = mnt[:f_flags] & MNT_VISFLAGMASK
+
+          # BUG: Seems to repeat some values
+          @@opt_names.each{ |key,val|
+            if flags & key > 0
+              if opts.empty?
+                opts = val
+              else
+                opts << ",#{val}"
+              end
+              flags &= ~key
+            end
+          }
+
+          obj.options = opts
 
           if block_given?
             yield obj.freeze
