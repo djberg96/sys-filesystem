78229574eefa54c1825575faf3b6425652865e4f
Daniel Berger <djberg96@gmail.com>
2012-01-11 01:41:29 -0700

    Modify wrapper file in preparation for code split.

--- lib/sys/filesystem.rb
+++ lib/sys/filesystem.rb
@@ -1,5 +1,14 @@
-if File::ALT_SEPARATOR
-  require 'windows/sys/filesystem'
-else
-  require 'unix/sys/filesystem'
+require 'rbconfig'
+
+case RbConfig::CONFIG['host_os']
+  when /windows|mswin|mingw|win32|dos/i
+    require 'windows/sys/filesystem'
+  when /bsd/i
+    require 'bsd/sys/filesystem'
+  when /darwin|osx|mach/i
+    require 'darwin/sys/filesystem'
+  when /sunos|solaris/i
+    require 'sunos/sys/filesystem'
+  else
+    require 'unix/sys/filesystem'
 end
