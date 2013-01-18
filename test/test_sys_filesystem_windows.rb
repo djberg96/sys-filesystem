4ad8cb0c95bbc26f0c17fa212c788ced0e58de38
Daniel Berger <djberg96@gmail.com>
2013-01-18 16:51:07 -0700

    Begin rework of Windows tests.

--- test/test_sys_filesystem_windows.rb
+++ test/test_sys_filesystem_windows.rb
@@ -4,95 +4,102 @@
 # Test case for the Sys::Filesystem.stat method and related stuff.
 # This should be run via the 'rake test' task.
 ####################################################################
-require 'rubygems'
-gem 'test-unit'
-
-require 'test/unit'
+require 'test-unit'
 require 'sys/filesystem'
 require 'rbconfig'
 include Sys
 
 class TC_Sys_Filesystem_Windows < Test::Unit::TestCase
-   def setup
-      @dir   = '/'
-      @stat  = Filesystem.stat(@dir)
-      @mount = Filesystem.mounts[0]
-      @size  = 58720256
-      @array = []
-   end
+  def setup
+    @dir   = '/'
+    @stat  = Filesystem.stat(@dir)
+    #@mount = Filesystem.mounts[0]
+    @size  = 58720256
+    @array = []
+  end
 
-   def test_version
-      assert_equal('1.0.0', Filesystem::VERSION)
-   end
+  test "version number is set to the expected value" do
+    assert_equal('1.1.0', Filesystem::VERSION)
+  end
 
-   def test_stat_path
-      assert_respond_to(@stat, :path)
-      assert_equal("/", @stat.path)
-   end
+  test "stat path works as expected" do
+    assert_respond_to(@stat, :path)
+    assert_equal("/", @stat.path)
+  end
 
-   def test_stat_block_size
-      assert_respond_to(@stat, :block_size)
-      assert_kind_of(Fixnum, @stat.block_size)
-   end
+  test "stat block_size works as expected" do
+    assert_respond_to(@stat, :block_size)
+    assert_kind_of(Fixnum, @stat.block_size)
+  end
 
-   def test_stat_fragment_size
-      assert_respond_to(@stat, :fragment_size)
-      assert_nil(@stat.fragment_size)
-   end
+  test "stat fragment_size works as expected" do
+    assert_respond_to(@stat, :fragment_size)
+    assert_nil(@stat.fragment_size)
+  end
 
-   def test_stat_blocks
-      assert_respond_to(@stat, :blocks)
-      assert_kind_of(Fixnum, @stat.blocks)
-   end
+  test "stat blocks works as expected" do
+    assert_respond_to(@stat, :blocks)
+    assert_kind_of(Fixnum, @stat.blocks)
+  end
 
-   def test_stat_blocks_free
-      assert_respond_to(@stat, :blocks_free)
-      assert_kind_of(Fixnum, @stat.blocks_free)
-   end
+  test "stat blocks_free works as expected" do
+    assert_respond_to(@stat, :blocks_free)
+    assert_kind_of(Fixnum, @stat.blocks_free)
+  end
 
-   def test_stat_blocks_available
-      assert_respond_to(@stat, :blocks_available)
-      assert_kind_of(Fixnum, @stat.blocks_available)
-   end
+  test "stat blocks_available works as expected" do
+    assert_respond_to(@stat, :blocks_available)
+    assert_kind_of(Fixnum, @stat.blocks_available)
+  end
 
-   def test_stat_files
-      assert_respond_to(@stat, :files)
-      assert_respond_to(@stat, :inodes)            # Alias
-      assert_nil(@stat.files)
-   end
+  test "stat files works as expected" do
+    assert_respond_to(@stat, :files)
+    assert_nil(@stat.files)
+  end
 
-   def test_stat_files_free
-      assert_respond_to(@stat, :files_free)
-      assert_respond_to(@stat, :inodes_free)       # Alias
-      assert_nil(@stat.files_free)
-   end
+  test "stat inodes is an alias for files" do
+    assert_alias_method(@stat, :inodes, :files)
+  end
 
-   def test_stat_files_available
-      assert_respond_to(@stat, :files_available)
-      assert_respond_to(@stat, :inodes_available)  # Alias
-      assert_nil(@stat.files_available)
-   end
+  test "stat files_free works as expected" do
+    assert_respond_to(@stat, :files_free)
+    assert_nil(@stat.files_free)
+  end
 
-   def test_stat_filesystem_id
-      assert_respond_to(@stat, :filesystem_id)
-      assert_kind_of(Integer, @stat.filesystem_id)
-   end
+  test "stat inodes_free is an alias for files_free" do
+    assert_respond_to(@stat, :inodes_free)
+  end
 
-   def test_stat_flags
-      assert_respond_to(@stat, :flags)
-      assert_kind_of(Fixnum, @stat.flags)
-   end
+  test "stat files available works as expected" do
+    assert_respond_to(@stat, :files_available)
+    assert_nil(@stat.files_available)
+  end
 
-   def test_stat_name_max
-      assert_respond_to(@stat, :name_max)
-      assert_kind_of(Fixnum, @stat.name_max)
-   end
+  test "stat inodes_available is an alias for files_available" do
+    assert_alias_method(@stat, :inodes_available, :files_available)
+  end
 
-   def test_stat_base_type
-      assert_respond_to(@stat, :base_type)
-      assert_kind_of(String, @stat.base_type)
-   end
+  test "stat filesystem_id works as expected" do
+    assert_respond_to(@stat, :filesystem_id)
+    assert_kind_of(Integer, @stat.filesystem_id)
+  end
 
+  test "stat flags works as expected" do
+    assert_respond_to(@stat, :flags)
+    assert_kind_of(Fixnum, @stat.flags)
+  end
+
+  test "stat name_max works as expected" do
+    assert_respond_to(@stat, :name_max)
+    assert_kind_of(Fixnum, @stat.name_max)
+  end
+
+  test "stat base_type works as expected" do
+    assert_respond_to(@stat, :base_type)
+    assert_kind_of(String, @stat.base_type)
+  end
+
+=begin
    def test_mount_point_singleton
       assert_respond_to(Filesystem, :mount_point)
       assert_nothing_raised{ Filesystem.mount_point(Dir.pwd) }
@@ -132,7 +139,7 @@ class TC_Sys_Filesystem_Windows < Test::Unit::TestCase
       assert_kind_of(Array, Filesystem.mounts)
       assert_kind_of(Filesystem::Mount, Filesystem.mounts[0])
    end
- 
+
    def test_mounts_block_form
       assert_nil(Filesystem.mounts{})
       assert_nothing_raised{ Filesystem.mounts{ |mt| @array << mt }}
@@ -187,12 +194,13 @@ class TC_Sys_Filesystem_Windows < Test::Unit::TestCase
       assert_equal(56, @size.to_mb)
       assert_equal(0, @size.to_gb)
    end
+=end
 
-   def teardown
-      @array = nil
-      @dir   = nil
-      @stat  = nil
-      @size  = nil
-      @mount = nil
-   end
+  def teardown
+    @array = nil
+    @dir   = nil
+    @stat  = nil
+    @size  = nil
+    @mount = nil
+  end
 end
