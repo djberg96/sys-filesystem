aba9a1c4049aafac1c0891d1493f7eba1f8a695b
Daniel Berger <djberg96@gmail.com>
2019-10-14 22:25:58 -0400

    Bump version test.

--- test/test_sys_filesystem_windows.rb
+++ test/test_sys_filesystem_windows.rb
@@ -20,7 +20,7 @@ class TC_Sys_Filesystem_Windows < Test::Unit::TestCase
   end
 
   test "version number is set to the expected value" do
-    assert_equal('1.3.0', Filesystem::VERSION)
+    assert_equal('1.3.1', Filesystem::VERSION)
     assert_true(Filesystem::VERSION.frozen?)
   end
 
