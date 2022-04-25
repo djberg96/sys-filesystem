8d78e2cff12ff74f8f1cd4c21cf3fb2dd93e5ed2
Daniel Berger <djberg96@gmail.com>
2022-04-25 12:31:10 -0400

    Add some comments to windows helper modules.

--- lib/sys/windows/sys/filesystem/helper.rb
+++ lib/sys/windows/sys/filesystem/helper.rb
@@ -1,6 +1,9 @@
+# Reopen core Ruby classes here and add some custom methods.
 class String
   # Convenience method for converting strings to UTF-16LE for wide character
   # functions that require it.
+  #--
+  # TODO: Use a refinement.
   def wincode
     (tr(File::SEPARATOR, File::ALT_SEPARATOR) + 0.chr).encode('UTF-16LE')
   end
