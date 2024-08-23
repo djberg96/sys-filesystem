d8f65a53bca4449621403b318fc24f0555f18860
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-08-22 21:27:31 -0400

    Remove the freeze stuff, don't remember where that happened.

--- lib/sys/unix/sys/filesystem.rb
+++ lib/sys/unix/sys/filesystem.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'filesystem/constants'
 require_relative 'filesystem/structs'
 require_relative 'filesystem/functions'
@@ -43,13 +41,13 @@ module Sys
 
     # File used to read mount informtion from.
     if File.exist?('/etc/mtab')
-      MOUNT_FILE = '/etc/mtab'
+      MOUNT_FILE = '/etc/mtab'.freeze
     elsif File.exist?('/etc/mnttab')
-      MOUNT_FILE = '/etc/mnttab'
+      MOUNT_FILE = '/etc/mnttab'.freeze
     elsif File.exist?('/proc/mounts')
-      MOUNT_FILE = '/proc/mounts'
+      MOUNT_FILE = '/proc/mounts'.freeze
     else
-      MOUNT_FILE = 'getmntinfo'
+      MOUNT_FILE = 'getmntinfo'.freeze
     end
 
     private_constant :MOUNT_FILE
